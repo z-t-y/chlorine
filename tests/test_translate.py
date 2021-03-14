80987bf20157b615aebfb1601c1c2451d074aa0b
rice0208 <riceforever0208@outlook.com>
2021-03-14 09:18:52 +0800

    修复测试

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -3,9 +3,9 @@ from chlorine import RESERVED_KEYWORDS, Translate
 
 def test_reserved_keywords():
     assert "bilibili" in RESERVED_KEYWORDS
-    assert RESERVED_KEYWORDS == Translate.reserved_keywords()
+    assert RESERVED_KEYWORDS == Translate.keywords()
 
 
 def test_translate():
-    t = Translate("bilibili")
-    assert t.translate() == "bilibili"
+    t = Translate("抄饭咋做，轮胎波萝咋冲气")
+    assert t.translate() == "炒饭怎么做，轮胎菠萝怎么充气"
