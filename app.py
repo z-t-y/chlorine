237bfa25d2659436d17b6e247c88f026ffc3235a
rice0208 <riceforever0208@outlook.com>
2021-03-24 19:33:43 +0800

    将bootstrap下载到本地（解决JsDelivr无法访问）

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template, request, url_for, redirect
 from chlorine import Translate
+import time
 
 app = Flask(__name__)
 
@@ -9,6 +10,8 @@ app = Flask(__name__)
 def api():
     if request.args['word'] is not None:
         word = request.args['word']
+
+        print(str(time.time())+'    word='+word+'    trans='+Translate(word).translate(), file=log)
         return {word: Translate(word).translate()}
 
 @app.route('/',methods=('GET','POST'))
@@ -17,11 +20,11 @@ def index() -> 'html':
         word = request.form['input']
         return redirect(url_for('trans',word=word))
     return render_template('base.html',
-                           the_title='首页',
-                           word='Welcome to Fluorine!',
-                           word_trans='在页面最下方输入内容，返回结果！',
-                           text_content='你好，我们是Fluorine团队！',
-                           )
+                        the_title='首页',
+                        word='Welcome to Fluorine!',
+                        word_trans='在页面最下方输入内容，返回结果！',
+                        text_content='你好，我们是Fluorine团队！',
+                        )
 
 @app.route('/trans',methods=('GET','POST'))
 def trans() -> 'html':
@@ -34,11 +37,11 @@ def trans() -> 'html':
         word = str(request.form['input'])
         return redirect(url_for('trans',word=word))
     return render_template('base.html',
-                           the_title='转换',
-                           word='转换结果',
-                           word_trans='转换结果：'+Translate(word_).translate(),
-                           text_content=word_,
-                           )
+                        the_title='转换',
+                        word='转换结果',
+                        word_trans='转换结果：'+Translate(word_).translate(),
+                        text_content=word_,
+                        )
 
 if __name__ == '__main__':
     app.run(debug=True)
